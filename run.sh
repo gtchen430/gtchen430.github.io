f3747eb883ade3ab78f547adc100bd98cee0b06e
gtchen430 <106620469+gtchen430@users.noreply.github.com>
2022-10-30 12:40:02 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#! usr/bin/bash
+python jemdoc.py -c analytics.conf index
+python jemdoc.py -c analytics.conf about
+python jemdoc.py -c analytics.conf pubs
+python jemdoc.py -c analytics.conf intern
+python jemdoc.py -c analytics.conf talks
+python jemdoc.py -c analytics.conf awards
+python jemdoc.py -c analytics.conf service
+python jemdoc.py -c analytics.conf mentor
+python jemdoc.py -c analytics.conf thesis
+python jemdoc.py -c analytics.conf contact
+# python jemdoc.py -c analytics.conf nlpprojs
+# python jemdoc.py -c analytics.conf mlprojs
